60db083a73bd1ae07ef1b591b4f5bcac16c3a6e5
LN710Q <LN710Q@DEILABO1-PC17>
2018-06-14 13:01:34 -0600

    Se subio avance hecho en labo

---
+++ src/modelo/Filtro.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class Filtro {
+    private int id;
+    private String codigo;
+    private String marca;
+    private int stock;
+    private boolean exixtencia;
+    
+    public Filtro() {
+    }
+
+    public Filtro(int id, String codigo, String marca, int stock, boolean exixtencia) {
+        this.id = id;
+        this.codigo = codigo;
+        this.marca = marca;
+        this.stock = stock;
+        this.exixtencia = exixtencia;
+    }
+
+    public Filtro(String codigo, String marca, boolean exixtencia) {
+        this.codigo = codigo;
+        this.marca = marca;
+        this.exixtencia = exixtencia;
+    }
+
+    public Filtro(String marca, int stock, boolean exixtencia) {
+        this.marca = marca;
+        this.stock = stock;
+        this.exixtencia = exixtencia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean getExixtencia() {
+        return exixtencia;
+    }
+
+    public void setExixtencia(boolean exixtencia) {
+        this.exixtencia = exixtencia;
+    }
+    
+    
+}
